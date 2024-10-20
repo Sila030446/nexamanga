b94e4b89924118319b60963a82c0abbe43f4e09b
SilaPrtnw. <99876548+Sila030446@users.noreply.github.com>
2024-10-20 16:36:20 +0700

    add nextjs-toploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,12 @@ import Provider from "./provider";
 import Authenticated from "./(auth)/authenticated";
 import { Kanit } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "Nexamanga อ่าน Manga ออนไลน์",
   description:
-    "อ่าน เขียน แปล นิยายและการ์ตูนยุคใหม่ทุกที่ทุกเวลา อัปเดตต่อเนื่องทุกวัน คลังนิยายและการ์ตูนที่ใหญ่ที่สุด นิยายจีน นิยายเกาหลี การ์ตูนจีน การ์ตูนเกาหลี มังงะ มังฮวา หลากหลายเรื่อง หลากหลายอารมณ์ ขับเคลื่อนและพัฒนาโดยชุมชน 100%",
+    "อ่าน เขียน แปล นิยายและการ์ตูนยุคใหม่ทุกที่ทุกเวลา อัปเดตต่อเนื่องทุกวัน คลังนิยายและการ์ตูนที่ใหญ่ที่สุด นิยายจีน นิยายเกาหลี การ์ตูนจีน การ์ตูนเกาหลี มังงะ มังฮวา หลากหลายเรื่อง หลากหลายอารมณ์",
 };
 
 const kanit = Kanit({
@@ -26,7 +27,10 @@ export default async function RootLayout({
     <html lang="en">
       <body className={`antialiased ${kanit.className}`}>
         <Analytics />
-        <Provider authenticated={isAuthenticated}>{children}</Provider>
+        <Provider authenticated={isAuthenticated}>
+          <NextTopLoader />
+          {children}
+        </Provider>
       </body>
     </html>
   );
